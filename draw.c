08b46500f8732c4fc419b939080ed8233bbfaaf0
Lauren <lpehlivanian00@stuy.edu>
2020-02-11 01:22:42 -0500

    finished assignment

--- draw.c
+++ draw.c
@@ -5,56 +5,167 @@
 #include "display.h"
 #include "draw.h"
 
-//http://www.uobabylon.edu.iq/eprints/publication_2_22893_6215.pdf
+// void switch(int x0, int y0, int x1, int y1) {
+//   int temp = y1;
+//   y1 = y0;
+//   y0 = temp;
+//   temp = x1;
+//   x1 = x0;
+//   x0 = temp;
+// }
 
 //Insert your line algorithm here
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-  //get general line equation
-  int A = y1 - y0;
-  int B = -1 * (x1-x0);
 
   //get which quadrant we are in
-  int oct;
-  if ((y1 > y0 && x1 > x0) || (y1 < y0 && x1 < x0)) {
+  int oct=0;
+  int dx = x1-x0;
+  int dy = y1-y0;
+
+  //vertical line:
+  if (x1 == x0) {
+    if (y1 > y0) {
+      while (y0 <= y1) {
+        plot(s,c,x1,y0);
+        y0++;
+      }
+    } else {
+      while (y1 <= y0) {
+        plot(s,c,x1,y1);
+        y1++;
+      }
+    }
+
+  }
+
+  if ((dx>0 && dy>0 && dx>=dy) || (dx<0 && dy<0 && dx<=dy)) {
     oct = 1;
     //swap (x0,y0) and (x1,y1) if (x1,y1) isn't above
     if (y1 < y0) {
+      printf("switchng 0: (%d,%d)\t1: (%d,%d)\n",x0,y0,x1,y1);
       int temp = y1;
       y1 = y0;
       y0 = temp;
       temp = x1;
       x1 = x0;
-      x0 = x1;
+      x0 = temp;// switch(x0,y0,x1,y1);
+      printf("new 0: (%d,%d)\t1: (%d,%d)\n",x0,y0,x1,y1);
     }
   }
+  else if ((dx>0 && dy>0 && dx<=dy) || (dx<0 && dy<0 && dx>=dy)) {
+    oct = 2;
+    if (y1 < y0) {
+      printf("switchng 0: (%d,%d)\t1: (%d,%d)\n",x0,y0,x1,y1);
+      int temp = y1;
+      y1 = y0;
+      y0 = temp;
+      temp = x1;
+      x1 = x0;
+      x0 = temp;
+      // switch(x0,y0,x1,y1);
+      printf("new 0: (%d,%d)\t1: (%d,%d)\n",x0,y0,x1,y1);
+
+    }
+  }
+  else if ((dx<0 && dy>0 && -1*dx <= dy) || (dx>0 && dy<0 && -1*dy >= dx)) {
+    oct = 3;
+    if (y1 < y0) {
+      printf("switchng 0: (%d,%d)\t1: (%d,%d)\n",x0,y0,x1,y1);
+      int temp = y1;
+      y1 = y0;
+      y0 = temp;
+      temp = x1;
+      x1 = x0;
+      x0 = temp;
+      // switch(x0,y0,x1,y1);
+      printf("new 0: (%d,%d)\t1: (%d,%d)\n",x0,y0,x1,y1);
+    }
+  }
+  else {
+    oct = 4;
+    if (x1 > x0) {
+      printf("switchng 0: (%d,%d)\t1: (%d,%d)\n",x0,y0,x1,y1);
+      int temp = y1;
+      y1 = y0;
+      y0 = temp;
+      temp = x1;
+      x1 = x0;
+      x0 = temp;
+      // switch(x0,y0,x1,y1);
+      printf("new 0: (%d,%d)\t1: (%d,%d)\n",x0,y0,x1,y1);
+    }
+  }
+  //oct = 4;
+  printf("OCTANT: %d\n",oct);
 
 
   int x = x0;
   int y = y0;
 
+  //get general line equation
+  int A = y1 - y0;
+  int B = -1 * (x1-x0);
+
   int d;
 
-  //octant 1
-  d = 2*A + B;
-  while (x<=x1) {
-    plot(s,c,x,y);
-    if (d>0) {
-      y ++;
-      d += 2*B;
-    }
-    x ++;
-    d += 2*A;
-  }
-
-  //octant 2
-  d = A + 2*B;
-  while (y<=y1) {
-    plot(s,c,x,y);
-    if (d<0) {
+  //octant 1 + 5(0 < slope < 1)
+  if (oct == 1){
+    d = 2*A + B;
+    while (x<=x1) {
+      //printf("Plotting: (%d,%d)\n",x,y);
+      //printf("d: %d\tPlotting: (%d,%d)\n",d,x,y);
+      plot(s,c,x,y);
+      if (d>0) {
+        y ++;
+        d += 2*B;
+      }
       x ++;
       d += 2*A;
     }
-    y++;
-    d += 2*B;
+  }
+
+  //octant 2 + 6(1 < slope < infinity)
+  if (oct == 2){
+    //printf("plotting oct2 line\n");
+    d = A + 2*B;
+    while (y<=y1) {
+      plot(s,c,x,y);
+      if (d<0) {
+        x ++;
+        d += 2*A;
+      }
+      y++;
+      d += 2*B;
+    }
+  }
+
+  //octant 3 + 7
+  if (oct == 3) {
+    //printf("plotting oct3 line\n");
+    d = 2*B - A;
+    while (y <= y1) {
+      //printf("d: %d\tPlotting: (%d,%d)\n",d,x,y);
+      plot(s,c,x,y);
+      if (d>0) {
+        x --;
+        d -= 2*A;
+      }
+      y ++;
+      d += 2*B;
+    }
+  }
+
+  //octant 4 + 8
+  if (oct ==4){
+    d = B - 2*A;
+    while (x >= x1) {
+      plot(s,c,x,y);
+      if (d < 0) {
+        y ++;
+        d += 2*B;
+      }
+      x --;
+      d -= 2*A;
+    }
   }
 }
--- main.c
+++ main.c
@@ -11,49 +11,92 @@ int main() {
   screen s;
   color c;
 
-  c.red = 0;
-  c.green = 255;
-  c.blue = 0;
-
   printf("hi\n");
   clear_screen(s);
-  int i=0;
-  for (i=0;i<150;i++) {
-    plot(s,c,i,0);
-  }
+  // int i=0;
+  // for (i=0;i<150;i++) {
+  //   plot(s,c,i,0);
+  // }
   printf("hi!\n");
-  draw_line(0,0,130,20,s,c);
-  // //octants 1 and 5
-  draw_line(0, 0, XRES-1, YRES-1, s, c);
-  // draw_line(0, 0, XRES-1, YRES / 2, s, c);
-  // draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c);
-  //
-  // //octants 8 and 4
-  // c.blue = 255;
-  // draw_line(0, YRES-1, XRES-1, 0, s, c);
-  // draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
-  // draw_line(XRES-1, 0, 0, YRES/2, s, c);
-  //
-  // //octants 2 and 6
-  // c.red = 255;
-  // c.green = 0;
-  // c.blue = 0;
-  // draw_line(0, 0, XRES/2, YRES-1, s, c);
-  // draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
-  //
-  // //octants 7 and 3
-  // c.blue = 255;
-  // draw_line(0, YRES-1, XRES/2, 0, s, c);
-  // draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
-  //
-  // //horizontal and vertical
-  // c.blue = 0;
+
+  int left_x = 20;
+  int bot_y = 0;
+
+  while (bot_y+100 < YRES) {
+
+    //body
+    //brown
+    c.red = 139;
+    c.green = 69;
+    c.blue = 19;
+    draw_line(left_x,bot_y,left_x+300,bot_y,s,c);
+    draw_line(left_x,bot_y,left_x,bot_y+100,s,c);
+    draw_line(left_x+120,bot_y,left_x+120,bot_y+100,s,c);
+    draw_line(left_x+120,bot_y+60,left_x+300,bot_y+60,s,c);
+    draw_line(left_x+300,bot_y,left_x+300,bot_y+60,s,c);
+    //ears
+    draw_line(left_x,bot_y+100,left_x+20,bot_y+80,s,c);
+    draw_line(left_x+120,bot_y+100,left_x+100,bot_y+80,s,c);
+    //top of head
+    draw_line(left_x+20,bot_y+80,left_x+100,bot_y+80,s,c);
+    //whiskers
+    c.red = 255;
+    c.green = 255;
+    c.blue = 255;
+    draw_line(left_x+85,bot_y+40,left_x+135,bot_y+40,s,c);
+    draw_line(left_x+90,bot_y+50,left_x+130,bot_y+60,s,c);
+    draw_line(left_x+90,bot_y+30,left_x+130,bot_y+20,s,c);
+    draw_line(left_x-15,bot_y+40,left_x+35,bot_y+40,s,c);
+    draw_line(left_x-10,bot_y+60,left_x+30,bot_y+50,s,c);
+    draw_line(left_x-10,bot_y+20,left_x+30,bot_y+30,s,c);
+    //eyes
+    //sky blue
+    c.red = 135;
+    c.green = 206;
+    c.blue = 250;
+    draw_line(left_x+25,bot_y+65,left_x+35,bot_y+65,s,c);
+    draw_line(left_x+85,bot_y+65,left_x+95,bot_y+65,s,c);
+
+    bot_y+= 110;
+  }
+
+
+  //MR. DW IMAGE
+  // c.red = 0;
   // c.green = 255;
-  // draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
-  // draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
+  // c.blue = 0;
+  // //octants 1 and 5
+  //   draw_line(0, 0, XRES-1, YRES-1, s, c);
+  //   draw_line(0, 0, XRES-1, YRES / 2, s, c);
+  //   draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c);
+  //
+  //   //octants 8 and 4
+  //   c.blue = 255;
+  //   draw_line(0, YRES-1, XRES-1, 0, s, c);
+  //   draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
+  //   draw_line(XRES-1, 0, 0, YRES/2, s, c);
+  //
+  //   //octants 2 and 6
+  //   c.red = 255;
+  //   c.green = 0;
+  //   c.blue = 0;
+  //   draw_line(0, 0, XRES/2, YRES-1, s, c);
+  //   draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
+  //
+  //   //octants 7 and 3
+  //   c.blue = 255;
+  //   draw_line(0, YRES-1, XRES/2, 0, s, c);
+  //   draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
+  //
+  //   //horizontal and vertical
+  //   c.blue = 0;
+  //   c.green = 255;
+  //   draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
+  //   draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
 
   display(s);
   save_extension(s, "lines.png");
   save_ppm(s, "binary.ppm");
   save_ppm_ascii(s, "ascii.ppm");
+  return 0;
 }
